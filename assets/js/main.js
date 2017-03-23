cf09ff45a925f177e4475f79ffb1c46d564445d5
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-23 12:11:36 -0600

    primer intento

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+  var numero="";
+  var numero2="";
+  var numeros=[];
+
+  var obtenerNumero = function(num){
+    numero += num;
+    console.log(numero);
+    return numero;
+  }
+
+    numeros.push(obtenerNumero);
+    console.log(numero);
