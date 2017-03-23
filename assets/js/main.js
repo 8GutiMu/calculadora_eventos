60034f42b4996b15cee2da04708625a0559c5aa0
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-23 16:07:52 -0600

    nuevos intentos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
   var numero="";
   var numero2="";
-  var numeros=[];
+  var numero=[];
 
-  var obtenerNumero = function(num){
+function obtenerNumero(num){
     numero += num;
     console.log(numero);
+    document.getElementById("display").value = numero;
     return numero;
   }
 
-    numeros.push(obtenerNumero);
-    console.log(numero);
+  var guardar = function(){
+    
+    clear();
+    numeros2 = obtenerNumero();
+
+    return nuevo;
+  }
+
+var resultado = function(guardar ){
+  document.getElementById("display").value = total;
+}
+
+var clear = function(){
+  document.getElementById("display").value = "";
+}
